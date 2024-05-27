322ff99ef54ed964a1bf76be27a06ecac5011d0a
Kevin Asena <asenakevin@gmail.com>
2024-05-27 02:12:29 -0700

    Update

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
-"""Creates a unique FileStorage instance for the application"""
-
-from models.engine import file_storage
+from models.engine.file_storage import FileStorage
 
 
-storage = file_storage.FileStorage()
+storage = FileStorage()
+
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Module base_model
-
-This Module contains a definition for BaseModel Class
 """
-
+Module for the BaseModel class.
+"""
 import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
-    ''' BaseModel class '''
     def __init__(self, *args, **kwargs):
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -20,19 +22,19 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, time_format))
                 else:
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
 
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+        models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """
+	Update updated_at with the current datetime
+        """
         self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
         """
-        returns a dictionary containing all
+	returns a dictionary containing all
         keys/values of __dict__ of the instance
         """
         inst_dict = self.__dict__.copy()
@@ -43,17 +45,21 @@ class BaseModel:
         return inst_dict
 
     def __str__(self):
-        """should print/str representation of the BaseModel instance."""
+        """
+	should print/str representation of the BaseModel instance
+        """
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
+
 if __name__ == "__main__":
     my_model = BaseModel()
-    my_model.name = "My First Model"
+    my_model.name = "My_First_Model"
     my_model.my_number = 89
+    print(my_model.id)
     print(my_model)
-    my_model.save()
-    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
